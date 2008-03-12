d8d57e4376be70538c5c063f5a29bf9f04604f96
rtamarasinghe <rtamarasinghe@localhost>
2008-03-12 15:46:55 +0000

    Class to manage employe location history

--- lib/models/hrfunct/AllTests.php
+++ lib/models/hrfunct/AllTests.php
@@ -33,6 +33,7 @@ require_once 'EmpSimpleBenefitTest.php';
 require_once 'EmpLocationTest.php';
 require_once 'JobTitleHistoryTest.php';
 require_once 'SubDivisionHistoryTest.php';
+require_once 'LocationHistoryTest.php';
 
 
 class models_hrfunct_AllTests
@@ -50,6 +51,7 @@ class models_hrfunct_AllTests
 	    $suite->addTestSuite('EmpLocationTest');
 	    $suite->addTestSuite('JobTitleHistoryTest');
 	    $suite->addTestSuite('SubDivisionHistoryTest');
+	    $suite->addTestSuite('LocationHistoryTest');
         return $suite;
     }
 }
