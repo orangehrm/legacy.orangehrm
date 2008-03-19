467191e5bc4f9d94d3461c61be53fc798f6e2e6f
idhanu <idhanu@localhost>
2008-03-19 12:50:09 +0000

    Company Property Completed

--- lib/controllers/ViewController.php
+++ lib/controllers/ViewController.php
@@ -2576,8 +2576,16 @@ class ViewController {
                                 $form_creator->popArr['emplist'] = $empInfo->getListofEmployee();
                             }
 
+
                             $compProp = new CompProperty();
-                            $form_creator->popArr['properties'] = $compProp->getPropertyList();
+
+                            if(!isset($_GET['sortField']))
+                            {
+                                $_GET['sortField']=0;
+                                $_GET['sortOrder0']='ASC';
+                            }
+
+                            $form_creator->popArr['properties'] = $compProp->getPropertyList($_GET['sortField'],$_GET['sortOrder0']);
 
                             break;
 
