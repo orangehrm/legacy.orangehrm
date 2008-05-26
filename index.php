a872be311d2bfb53d4ac4addf1ceb31df094fdf2
rtamarasinghe <rtamarasinghe@localhost>
2008-05-26 23:46:15 +0000

    Updated version number to alpha.9

--- index.php
+++ index.php
@@ -1141,7 +1141,7 @@ function preloadAllImages() {
 <?php } ?>
 <table width="100%">
 <tr>
-<td align="center"><a href="http://www.orangehrm.com" target="_blank">OrangeHRM</a> ver 2.2.2.2 (ors 0.1-alpha.8) &copy; OrangeHRM Inc. 2005 - 2007 All rights reserved.</td>
+<td align="center"><a href="http://www.orangehrm.com" target="_blank">OrangeHRM</a> ver 2.2.2.2 (ors 0.1-alpha.9) &copy; OrangeHRM Inc. 2005 - 2007 All rights reserved.</td>
 </tr>
 </table>
 <script language="javascript">
