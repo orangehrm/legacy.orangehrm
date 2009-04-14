dd78ff221b40c1ea8be126495b79e1812468f7b1
rtamarasinghe <rtamarasinghe@localhost>
2009-04-14 03:46:31 +0000

    Escape username entry before echoing. (Already checked in to trunk as revision: 3424)

--- login.php
+++ login.php
@@ -283,7 +283,7 @@ body {
               <td align="right" class="bodyTXT"><?php echo $lang_login_LoginName; ?> : </td>
               <td>
 <?php		if(isset($_POST['txtUserName'])) {?>
-              <input name="txtUserName" type="text" class="loginTXT" size="10" value="<?php echo $_POST['txtUserName']?>">
+              <input name="txtUserName" type="text" class="loginTXT" size="10" value="<?php echo CommonFunctions::escapeHtml($_POST['txtUserName']); ?>">
 <?php		} else { ?>
               <input name="txtUserName" type="text" class="loginTXT" size="10" >
 <?php		} ?>
