3018076fdaa62fe818d1fd3119adcd0eac156162
lgtkaushalya <lgtkaushalya@localhost>
2012-02-13 07:02:18 +0000

    Localized Employee reports in time module

--- symfony/apps/orangehrm/lib/report/ohrmWidgets/ohrmWidgetProjectActivityList.php
+++ symfony/apps/orangehrm/lib/report/ohrmWidgets/ohrmWidgetProjectActivityList.php
@@ -56,10 +56,6 @@ class ohrmWidgetProjectActivityList extends sfWidgetForm implements ohrmEmbeddab
 $(document).ready(function() {
      var getActivitiesLink = '%s';
 
-     if($('#time_project_name').val() == -1){
-        $('#time_activity_name').html("<option value='-1'> All </option>");
-     }
-
      $('#time_project_name').change(function() {
         
         var projectId = $('#time_project_name').val();
@@ -128,9 +124,9 @@ EOF
         $activityList = $timesheetDao->getProjectActivitiesByPorjectId($projectId);
 
         if ($activityList != null) {
-            $activityNameList[-1] = "All";
+            $activityNameList[-1] = __("All");
         } else {
-            $activityNameList[null] = "--No Project Activities--";
+            $activityNameList[null] = "--".__("No Project Activities")."--";
         }
 
         return $activityNameList;
@@ -144,11 +140,11 @@ EOF
 
         $widgetSchema = $form->getWidgetSchema();
         $widgetSchema[$this->attributes['id']] = $this;
-        $label = ucwords(str_replace("_", " ", $this->attributes['id']));
+        $label = __(ucwords(str_replace("_", " ", $this->attributes['id'])));
         $validator = new sfValidatorString(array('required' => false));
         if ($this->attributes['required'] == "true") {
             $label .= "<span class='required'> * </span>";
-            $validator = new sfValidatorString(array('required' => true), array('required' => 'Add an activiy to view'));
+            $validator = new sfValidatorString(array('required' => true), array('required' => __('Add an activiy to view')));
         }
         $widgetSchema[$this->attributes['id']]->setLabel($label);
         $form->setValidator($this->attributes['id'], $validator);
