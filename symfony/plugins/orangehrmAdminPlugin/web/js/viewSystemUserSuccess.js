9535d4f3a132c9151f0f9c3a57a1f310088725ab
chameeracd <chameeracd@localhost>
2012-03-12 17:46:18 +0000

    bug 0000352 fixed

--- symfony/plugins/orangehrmAdminPlugin/web/js/viewSystemUserSuccess.js
+++ symfony/plugins/orangehrmAdminPlugin/web/js/viewSystemUserSuccess.js
@@ -53,24 +53,26 @@ $(document).ready(function() {
     
     $("#searchSystemUser_employeeName").autocomplete(employees, {
 
-            formatItem: function(item) {
-                           return item.name;
-                    }
-                    ,matchContains:true
-            }).result(function(event, item) {
-                $('#searchSystemUser_employeeId').val(item.id);
-            }
-	);
+        formatItem: function(item) {
+            return item.name;
+        }
+        ,
+        matchContains:true
+    }).result(function(event, item) {
+        $('#searchSystemUser_employeeId').val(item.id);
+    }
+    );
             
-     if ($("#searchSystemUser_employeeName").val() == '') {
+    if ($("#searchSystemUser_employeeName").val() == '') {
+        $('#searchSystemUser_employeeId').val('');
         $("#searchSystemUser_employeeName").val(lang_typeforhint)
         .addClass("inputFormatHint");
     }
     
-     $('#searchBtn').click(function() {
-         if(isValidForm()){         
+    $('#searchBtn').click(function() {
+        if(isValidForm()){         
             $('#search_form').submit();
-         }
+        }
     });
     
     $('#resetBtn').click(function() {
@@ -84,24 +86,30 @@ $(document).ready(function() {
     
     $('#searchSystemUser_employeeName').click(function(){
         if($('#searchSystemUser_employeeName').val() == lang_typeforhint){
+            $('#searchSystemUser_employeeId').val('');
             $('#searchSystemUser_employeeName').val("")
             $('#searchSystemUser_employeeName').removeClass("inputFormatHint");
         }
-        }); 
+    }); 
     
+    $('#searchSystemUser_employeeName').keyup(function(){
+        alert('xxx');
+        $('#searchSystemUser_employeeId').val('');
+    });
+
 });
 
 $.validator.addMethod("validEmployeeName", function(value, element) {                 
-               	 var empName    =   trim($('#searchSystemUser_employeeName').val());
-                 var empId      =   $('#searchSystemUser_employeeId').val();  
+    var empName    =   trim($('#searchSystemUser_employeeName').val());
+    var empId      =   $('#searchSystemUser_employeeId').val();  
                  
-                 if(empName != lang_typeforhint && empName.length > 0  && empId == ''){
-                     return false;
-                 }else{
-                     return true;
-                 }
+    if(empName != lang_typeforhint && empName.length > 0  && empId == ''){
+        return false;
+    }else{
+        return true;
+    }
                 
-            });
+});
             
 
 function addSystemUser(){
