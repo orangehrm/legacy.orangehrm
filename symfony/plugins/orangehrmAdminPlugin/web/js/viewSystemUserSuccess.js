d6b8fd8f5fe5f326585111b08915b15146b391d2
chameeracd <chameeracd@localhost>
2012-03-13 05:08:36 +0000

    unescape html in autocomplete

--- symfony/plugins/orangehrmAdminPlugin/web/js/viewSystemUserSuccess.js
+++ symfony/plugins/orangehrmAdminPlugin/web/js/viewSystemUserSuccess.js
@@ -93,7 +93,6 @@ $(document).ready(function() {
     }); 
     
     $('#searchSystemUser_employeeName').keyup(function(){
-        alert('xxx');
         $('#searchSystemUser_employeeId').val('');
     });
 
