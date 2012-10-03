d4fc6a20cc70fafc7aae5d2381e3fd5108376cc6
rtamarasinghe <rtamarasinghe@localhost>
2012-10-03 10:34:23 +0000

    upgrade leave module task: moves data from old leave tables to new

--- symfony/plugins/orangehrmLeavePlugin/install/upgrade.sql
+++ symfony/plugins/orangehrmLeavePlugin/install/upgrade.sql
@@ -1,13 +1,13 @@
 
 TRUNCATE TABLE ohrm_leave_entitlement;
-TRUNCATE TABLE ohrm_leave_type;
+DELETE FROM ohrm_leave_type;
 
-alter table `hs_hr_leave_type` add column int_id int not null auto_increment unique key;
+alter table `hs_hr_leavetype` add column int_id int not null auto_increment unique key;
 
 INSERT INTO `ohrm_leave_type` (`id`, `name`, `deleted`, `operational_country_id`)
-                    SELECT hs_hr_leavetype.`int_id`, hs_hr_leavetype.`leave_type_name`, 
-                    IF(hs_hr_leavetype.`available_flag` = 1, 0, 1 , hs_hr_leavetype.`operational_country_id`)
-                    FROM `hs_hr_leavetype`;
+                    SELECT old_lt.`int_id`, old_lt.`leave_type_name`, 
+                    IF(old_lt.`available_flag` = 1, 0, 1) , old_lt.`operational_country_id`
+                    FROM `hs_hr_leavetype` old_lt;
 
 INSERT INTO `ohrm_leave_entitlement`(emp_number, no_of_days, leave_type_id, from_date, to_date, 
                                 credited_date, note, entitlement_type, `deleted`)
@@ -15,3 +15,5 @@ INSERT INTO `ohrm_leave_entitlement`(emp_number, no_of_days, leave_type_id, from
             p.leave_period_start_date, 'record created by upgrade', 1, 0
             FROM `hs_hr_employee_leave_quota` q left join `hs_hr_leavetype` lt on lt.leave_type_id = q.leave_type_id
             left join hs_hr_leave_period p on p.leave_period_id = q.leave_period_id; 
+
+alter table `hs_hr_leavetype` drop column int_id;
