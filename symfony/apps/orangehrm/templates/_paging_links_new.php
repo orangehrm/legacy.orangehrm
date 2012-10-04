64ee8ffd0c106b85ea72f8661c75385134c5bf54
webgaya <webgaya@localhost>
2012-10-04 07:01:25 +0000

    Tooltipls for paging.

--- symfony/apps/orangehrm/templates/_paging_links_new.php
+++ symfony/apps/orangehrm/templates/_paging_links_new.php
@@ -19,8 +19,8 @@
  *
  */
 
-echo "<li class=\"first\"><a href=\"javascript:submitPage(1)\" class=\"tiptip\">".__('First')."</a></li> ";
-echo "<li class=\"previous\"><a href=\"javascript:submitPage({$pager->getPreviousPage()})\" class=\"tiptip\">".__('Previous')."</a></li> ";
+echo "<li class=\"first\"><a href=\"javascript:submitPage(1)\" class=\"tiptip\" title=\"".__('First')."\">".__('First')."</a></li> ";
+echo "<li class=\"previous\"><a href=\"javascript:submitPage({$pager->getPreviousPage()})\" class=\"tiptip\" title=\"".__('Previous')."\">".__('Previous')."</a></li> ";
 
 
 foreach ($pager->getLinks() as $page):
@@ -33,5 +33,5 @@ foreach ($pager->getLinks() as $page):
     
 endforeach;
 
-echo "<li class=\"next\"><a href=\"javascript:submitPage({$pager->getNextPage()})\" class=\"tiptip\">".__('Next')."</a></li> ";
-echo "<li class=\"last\"><a href=\"javascript:submitPage({$pager->getLastPage()})\" class=\"tiptip\">".__('Last')."</a></li>";
+echo "<li class=\"next\"><a href=\"javascript:submitPage({$pager->getNextPage()})\" class=\"tiptip\" title=\"".__('Next')."\">".__('Next')."</a></li> ";
+echo "<li class=\"last\"><a href=\"javascript:submitPage({$pager->getLastPage()})\" class=\"tiptip\" title=\"".__('Last')."\">".__('Last')."</a></li>";
