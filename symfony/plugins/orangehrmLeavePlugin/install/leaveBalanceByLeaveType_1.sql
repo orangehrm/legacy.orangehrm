7df09235a87784735a0ca8cd573fac9803105d5a
rtamarasinghe <rtamarasinghe@localhost>
2012-11-08 10:32:24 +0000

    leave balance report by leave type updated

--- symfony/plugins/orangehrmLeavePlugin/install/leaveBalanceByLeaveType_1.sql
+++ symfony/plugins/orangehrmLeavePlugin/install/leaveBalanceByLeaveType_1.sql
@@ -12,6 +12,7 @@ INSERT INTO ohrm_advanced_report (id, name, definition) VALUES
 	<input_field type="text" name="leaveType" label="Leave Type"></input_field>
 	<input_field type="text" name="fromDate" label="From"></input_field>
         <input_field type="text" name="toDate" label="To"></input_field>
+        <input_field type="text" name="asOfDate" label="AsOf"></input_field>
 </filter_fields> 
 
 <sub_report type="sql" name="mainTable">       
@@ -38,11 +39,31 @@ INSERT INTO ohrm_advanced_report (id, name, definition) VALUES
     </display_groups> 
 </sub_report>
 
-<sub_report type="sql" name="subTable1">
+<sub_report type="sql" name="entitlementsAsOf">
                     <query>
-FROM ohrm_leave_entitlement where $X{=,leave_type_id,leaveType}
-GROUP BY emp_number
-ORDER BY emp_number
+
+FROM (
+SELECT ohrm_leave_entitlement.id as id, 
+       ohrm_leave_entitlement.emp_number as emp_number,
+       ohrm_leave_entitlement.no_of_days as no_of_days,
+       sum(IF(ohrm_leave.status = 2, ohrm_leave_leave_entitlement.length_days, 0)) AS scheduled,
+       sum(IF(ohrm_leave.status = 3, ohrm_leave_leave_entitlement.length_days, 0)) AS taken
+       
+FROM ohrm_leave_entitlement LEFT JOIN ohrm_leave_leave_entitlement ON
+    ohrm_leave_entitlement.id = ohrm_leave_leave_entitlement.entitlement_id
+    LEFT JOIN ohrm_leave ON ohrm_leave.id = ohrm_leave_leave_entitlement.leave_id AND 
+    ( $X{&gt;,ohrm_leave.date,toDate} OR $X{&lt;,ohrm_leave.date,fromDate} )
+
+WHERE 
+    $X{=,ohrm_leave_entitlement.leave_type_id,leaveType} AND
+    $X{&lt;=,ohrm_leave_entitlement.from_date,asOfDate} AND
+    $X{&gt;=,ohrm_leave_entitlement.to_date,asOfDate} 
+    
+GROUP BY ohrm_leave_entitlement.id
+) AS A
+
+GROUP BY A.emp_number
+ORDER BY A.emp_number
 
 </query>
     <id_field>empNumber</id_field>
@@ -51,43 +72,15 @@ ORDER BY emp_number
                 <group_header></group_header>
                 <fields>
                     <field display="false">
-                        <field_name>ohrm_leave_entitlement.emp_number</field_name>
+                        <field_name>A.emp_number</field_name>
                         <field_alias>empNumber</field_alias>
                         <display_name>Emp Number</display_name>
                         <width>1</width>
-                    </field>                                
+                    </field>                          
                     <field display="true">
-                        <field_name>SUM(no_of_days)</field_name>
+                        <field_name>sum(A.no_of_days) - sum(A.scheduled) - sum(A.taken)</field_name>
                         <field_alias>entitlement</field_alias>
-                        <display_name>Opening Balance</display_name>
-                        <width>120</width>
-                    </field>                                
-                </fields>
-            </display_group>
-    </display_groups>
-    </sub_report>
-<sub_report type="sql" name="subTable2">
-                    <query>
-FROM ohrm_leave_entitlement WHERE $X{=,leave_type_id,leaveType} AND
-from_date &lt; curdate()
-GROUP BY emp_number
-ORDER BY emp_number
-</query>
-    <id_field>empNumber</id_field>
-    <display_groups>
-            <display_group name="g2" type="one" display="true">
-                <group_header></group_header>
-                <fields>
-                    <field display="false">
-                        <field_name>ohrm_leave_entitlement.emp_number</field_name>
-                        <field_alias>empNumber</field_alias>
-                        <display_name>Emp Number</display_name>
-                        <width>1</width>
-                    </field>                                
-                    <field display="true">
-                        <field_name>SUM(no_of_days)</field_name>
-                        <field_alias>earned</field_alias>
-                        <display_name>Entitlements Earned</display_name>
+                        <display_name>Entitlments valid as of</display_name>
                         <width>120</width>
                     </field>                                
                 </fields>
@@ -95,16 +88,111 @@ ORDER BY emp_number
     </display_groups>
     </sub_report>
 
-<sub_report type="sql" name="subTable3">
+<sub_report type="sql" name="entitlementsTotal">
+                    <query>
+
+FROM (
+SELECT ohrm_leave_entitlement.id as id, 
+       ohrm_leave_entitlement.emp_number as emp_number,
+       ohrm_leave_entitlement.no_of_days as no_of_days,
+       sum(IF(ohrm_leave.status = 2, ohrm_leave_leave_entitlement.length_days, 0)) AS scheduled,
+       sum(IF(ohrm_leave.status = 3, ohrm_leave_leave_entitlement.length_days, 0)) AS taken
+       
+FROM ohrm_leave_entitlement LEFT JOIN ohrm_leave_leave_entitlement ON
+    ohrm_leave_entitlement.id = ohrm_leave_leave_entitlement.entitlement_id
+    LEFT JOIN ohrm_leave ON ohrm_leave.id = ohrm_leave_leave_entitlement.leave_id AND 
+    ( $X{&gt;,ohrm_leave.date,toDate} OR $X{&lt;,ohrm_leave.date,fromDate} )
+
+WHERE $X{=,ohrm_leave_entitlement.leave_type_id,leaveType} AND 
+    (
+      ( $X{&lt;=,ohrm_leave_entitlement.from_date,fromDate} AND $X{&gt;=,ohrm_leave_entitlement.to_date,fromDate} ) OR
+      ( $X{&lt;=,ohrm_leave_entitlement.from_date,toDate} AND $X{&gt;=,ohrm_leave_entitlement.to_date,toDate} ) OR 
+      ( $X{&gt;=,ohrm_leave_entitlement.from_date,fromDate} AND $X{&lt;=,ohrm_leave_entitlement.to_date,toDate} ) 
+    )
+    
+GROUP BY ohrm_leave_entitlement.id
+) AS A
+
+GROUP BY A.emp_number
+ORDER BY A.emp_number
+
+</query>
+    <id_field>empNumber</id_field>
+    <display_groups>
+            <display_group name="g2" type="one" display="true">
+                <group_header></group_header>
+                <fields>
+                    <field display="false">
+                        <field_name>A.emp_number</field_name>
+                        <field_alias>empNumber</field_alias>
+                        <display_name>Emp Number</display_name>
+                        <width>1</width>
+                    </field>                                
+                    <field display="true">
+                        <field_name>sum(A.no_of_days) - sum(A.scheduled) - sum(A.taken)</field_name>
+                        <field_alias>entitlement_total</field_alias>
+                        <display_name>Total entitlements valid for period</display_name>
+                        <width>120</width>
+                    </field>                                
+                </fields>
+            </display_group>
+    </display_groups>
+</sub_report>
+
+<sub_report type="sql" name="balanceQuery">
 <query>
-FROM ohrm_leave WHERE $X{=,leave_type_id,leaveType} AND
-status = 3
+FROM (
+SELECT le.emp_number AS emp_number,
+       le.no_of_days AS entitled, 
+       sum(IF(l.status = 2, lle.length_days, 0)) AS scheduled, 
+       sum(IF(l.status = 3, lle.length_days, 0)) AS taken 
+FROM ohrm_leave_entitlement le LEFT JOIN 
+     ohrm_leave_leave_entitlement lle ON le.id = lle.entitlement_id LEFT JOIN
+     ohrm_leave l ON l.id = lle.leave_id
+WHERE le.deleted = 0 AND $X{=,le.leave_type_id,leaveType} AND 
+      $X{&lt;=,le.from_date,asOfDate} AND
+      $X{&gt;=,le.to_date,asOfDate} 
+
+GROUP BY le.id
+) AS B
+
+GROUP BY B.emp_number
+ORDER BY B.emp_number
+
+</query>
+    <id_field>empNumber</id_field>
+    <display_groups>
+            <display_group name="g3" type="one" display="true">
+                <group_header></group_header>
+                <fields>
+                    <field display="false">
+                        <field_name>B.emp_number</field_name>
+                        <field_alias>empNumber</field_alias>
+                        <display_name>Emp Number</display_name>
+                        <width>1</width>
+                    </field>                                
+                    <field display="true">
+                        <field_name>sum(B.entitled) - sum(B.scheduled) - sum(B.taken)</field_name>
+                        <field_alias>balance</field_alias>
+                        <display_name>Leave Balance as of Date</display_name>
+                        <width>120</width>
+                    </field>                                
+                </fields>
+            </display_group>
+    </display_groups>
+</sub_report>
+
+<sub_report type="sql" name="scheduledQuery">
+<query>
+FROM ohrm_leave WHERE $X{=,ohrm_leave.leave_type_id,leaveType} AND
+status = 2 AND
+$X{&gt;=,ohrm_leave.date,fromDate} AND $X{&lt;=,ohrm_leave.date,toDate}
 GROUP BY emp_number
 ORDER BY ohrm_leave.emp_number
 </query>
     <id_field>empNumber</id_field>
     <display_groups>
-            <display_group name="g3" type="one" display="true">
+            <display_group name="g5" type="one" display="true">
                 <group_header></group_header>
                 <fields>
                     <field display="false">
@@ -114,20 +202,21 @@ ORDER BY ohrm_leave.emp_number
                         <width>1</width>
                     </field>                                
                     <field display="true">
-                        <field_name>SUM(length_days)</field_name>
-                        <field_alias>taken</field_alias>
-                        <display_name>Leave Taken</display_name>
+                        <field_name>sum(length_days)</field_name>
+                        <field_alias>scheduled</field_alias>
+                        <display_name>Leave Scheduled</display_name>
                         <width>120</width>
                     </field>                                
                 </fields>
             </display_group>
     </display_groups>
-    </sub_report>
+</sub_report>
 
-<sub_report type="sql" name="subTable4">
+<sub_report type="sql" name="takenQuery">
 <query>
-FROM ohrm_leave WHERE $X{=,leave_type_id,leaveType} AND
-status = 3
+FROM ohrm_leave WHERE $X{=,ohrm_leave.leave_type_id,leaveType} AND
+status = 3 AND
+$X{&gt;=,ohrm_leave.date,fromDate} AND $X{&lt;=,ohrm_leave.date,toDate}
 GROUP BY emp_number
 ORDER BY ohrm_leave.emp_number
 </query>
@@ -143,51 +232,23 @@ ORDER BY ohrm_leave.emp_number
                         <width>1</width>
                     </field>                                
                     <field display="true">
-                        <field_name>SUM(length_days)</field_name>
-                        <field_alias>scheduled</field_alias>
-                        <display_name>Leave Scheduled</display_name>
+                        <field_name>sum(length_days)</field_name>
+                        <field_alias>taken</field_alias>
+                        <display_name>Leave Taken</display_name>
                         <width>120</width>
                     </field>                                
                 </fields>
             </display_group>
     </display_groups>
-    </sub_report>
-
-<sub_report type="sql" name="subTable5">
-                    <query>
-FROM ohrm_leave_entitlement where $X{=,leave_type_id,leaveType} 
-GROUP BY emp_number
-ORDER BY ohrm_leave_entitlement.emp_number
-</query>
-    <id_field>empNumber</id_field>
-    <display_groups>
-            <display_group name="g6" type="one" display="true">
-                <group_header></group_header>
-                <fields>
-                    <field display="false">
-                        <field_name>ohrm_leave_entitlement.emp_number</field_name>
-                        <field_alias>empNumber</field_alias>
-                        <display_name>Emp Number</display_name>
-                        <width>1</width>
-                    </field>                                
-                    <field display="true">
-                        <field_name>SUM(no_of_days)</field_name>
-                        <field_alias>closing</field_alias>
-                        <display_name>Closing Balance</display_name>
-                        <width>120</width>
-                    </field>                                
-                </fields>
-            </display_group>
-    </display_groups>
-    </sub_report>
+</sub_report>
 
     <join>             
         <join_by sub_report="mainTable" id="empNumber"></join_by>
-        <join_by sub_report="subTable1" id="empNumber"></join_by>               
-        <join_by sub_report="subTable2" id="empNumber"></join_by>   
-        <join_by sub_report="subTable3" id="empNumber"></join_by>  
-        <join_by sub_report="subTable4" id="empNumber"></join_by>  
-        <join_by sub_report="subTable5" id="empNumber"></join_by> 
+        <join_by sub_report="entitlementsAsOf" id="empNumber"></join_by>               
+        <join_by sub_report="entitlementsTotal" id="empNumber"></join_by> 
+        <join_by sub_report="balanceQuery" id="empNumber"></join_by>   
+        <join_by sub_report="takenQuery" id="empNumber"></join_by>  
+        <join_by sub_report="scheduledQuery" id="empNumber"></join_by>
     </join>
     <page_limit>20</page_limit>
     <decorators>
