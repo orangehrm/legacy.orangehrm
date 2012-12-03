bc1852540b3ccdbb93adc17801381d6984fadd01
webgaya <webgaya@localhost>
2012-12-03 07:05:16 +0000

    Improvements to sfWidgetFormSchemaFormatterSingleColumnForm

--- symfony/apps/orangehrm/lib/form/widget/sfWidgetFormSchemaFormatterSingleColumnForm.php
+++ symfony/apps/orangehrm/lib/form/widget/sfWidgetFormSchemaFormatterSingleColumnForm.php
@@ -2,31 +2,12 @@
 
 class sfWidgetFormSchemaFormatterSingleColumnForm extends sfWidgetFormSchemaFormatter {
 
-    protected static $index = 1;
-    protected static $noOfColumns = 1;
     protected $rowFormat = "<li>%label%  %field%%help%\n%hidden_fields%</li> %error%\n";
     protected $errorRowFormat = "<span>\n%errors%</span>\n";
     protected $helpFormat = '<br />%help%';
     protected $decoratorFormat = "<form>\n  %content%</form>";
 
     /**
-     *
-     * @return int
-     */
-    public static function getNoOfColumns() {
-        return self::$noOfColumns;
-    }
-
-    /**
-     *
-     * @param int $noOfColumns 
-     */
-    public static function setNoOfColumns($noOfColumns) {
-        self::$noOfColumns = $noOfColumns;
-    }
-
-    /**
-     *
      * @param string $label
      * @param string $field
      * @param array $errors
