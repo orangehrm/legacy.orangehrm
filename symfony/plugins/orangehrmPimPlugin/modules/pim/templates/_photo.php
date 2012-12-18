b80351a035e945e5f9834b2ffdbc43f9a472ea20
webgaya <webgaya@localhost>
2012-12-18 10:38:05 +0000

    Improvements to PIM image holder.

--- symfony/plugins/orangehrmPimPlugin/modules/pim/templates/_photo.php
+++ symfony/plugins/orangehrmPimPlugin/modules/pim/templates/_photo.php
@@ -3,15 +3,19 @@
 <h1><?php echo $fullName; ?></h1>
 
 <?php if ($photographPermissions->canUpdate() || $photographPermissions->canDelete()) : ?>
+<div class="imageHolder">
 <a href="<?php echo url_for('pim/viewPhotograph?empNumber=' . $empNumber); ?>" title="<?php echo __('Change Photo'); ?>" class="tiptip">
     <img alt="Employee Photo" src="<?php echo url_for("pim/viewPhoto?empNumber=". $empNumber); ?>" border="0" id="empPic" 
          width="<?php echo $width; ?>" height="<?php echo $height; ?>"/>
 </a>
+</div>    
 <?php else: ?>
+<div class="imageHolder">
 <a href="#">
     <img alt="Employee Photo" src="<?php echo url_for("pim/viewPhoto?empNumber=". $empNumber); ?>" border="0" id="empPic" 
      width="<?php echo $width; ?>" height="<?php echo $height; ?>"/>
 </a>
+</div>    
 <?php endif; ?>
 
 </div> <!-- profile-pic -->
