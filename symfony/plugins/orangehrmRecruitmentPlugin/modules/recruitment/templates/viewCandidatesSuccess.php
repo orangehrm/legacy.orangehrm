faff44f521b1b5b7085fab485cfbbde74f19858e
webgaya <webgaya@localhost>
2012-12-19 15:03:00 +0000

    Improvements to view candidates.

--- symfony/plugins/orangehrmRecruitmentPlugin/modules/recruitment/templates/viewCandidatesSuccess.php
+++ symfony/plugins/orangehrmRecruitmentPlugin/modules/recruitment/templates/viewCandidatesSuccess.php
@@ -22,6 +22,13 @@
 <?php use_stylesheets_for_form($form); ?>
 <?php use_javascript('../orangehrmRecruitmentPlugin/js/viewCandidatesSuccess'); ?>
 
+<style type="text/css">
+    .searchForm form ol li span.validation-error {
+        left: 24px;
+    }
+</style>
+
+
 <div class="box searchForm toggableForm" id="srchCandidates">
     <div class="head">
         <h1><?php echo __('Candidates'); ?></h1>
