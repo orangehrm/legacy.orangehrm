8c629c1b488aaffee0213511b762c5afb317b91a
rtamarasinghe <rtamarasinghe@localhost>
2013-03-06 00:24:26 +0000

    remove debug print statements

--- symfony/plugins/orangehrmCorePlugin/lib/report/SqlDataGenerator.php
+++ symfony/plugins/orangehrmCorePlugin/lib/report/SqlDataGenerator.php
@@ -63,7 +63,6 @@ class SqlDataGenerator extends DataGenerator {
             throw new ReportException("Both offset and page limit should have a value or both should not have a value!!!!");
         }
 
-        print_r($query); print_r($parameters);
         $results = $this->executeQuery($query, $parameters);
         return $results;
     }
