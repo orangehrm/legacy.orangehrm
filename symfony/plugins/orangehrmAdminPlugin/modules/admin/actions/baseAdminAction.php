b15420ff8ade98eb929425e77eee47efdb93c91e
orangehrmnirmal <orangehrmnirmal@localhost>
2013-05-21 10:22:07 +0000

    base admin action added

---
+++ symfony/plugins/orangehrmAdminPlugin/modules/admin/actions/baseAdminAction.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of baseAdminAction
+ *
+ * @author nirmal
+ */
+abstract class baseAdminAction extends sfAction{
+    //put your code here
+}
+
+?>
