1c91267b8807c96a4aa558416c24e370fc4a0be7
orangehrmnirmal <orangehrmnirmal@localhost>
2013-05-30 05:33:41 +0000

    attendance configuration related authorization chages

--- dbscript/dbscript-2.sql
+++ dbscript/dbscript-2.sql
@@ -1620,7 +1620,8 @@ INSERT INTO `ohrm_data_group` (`id`, `name`, `description`, `can_read`, `can_cre
 (43, 'pay_grades', 'Admin - Pay Grades', 1, 1, 1, 1),
 (44, 'time_customers', 'Time - Customers', 1, 1, 1, 1),
 (45, 'time_projects', 'Time - Projects', 1, 1, 1, 1),
-(46, 'pim_reports', 'PIM - Reports', 1, 1, 1, 1);
+(46, 'pim_reports', 'PIM - Reports', 1, 1, 1, 1),
+(47, 'attendance_configuration', 'Attendance - Configuration', 1, 0, 1, 0);
 
 INSERT INTO `ohrm_user_role_data_group` (`user_role_id`, `data_group_id`, `can_read`, `can_create`, `can_update`, `can_delete`, `self`) VALUES
 (1, 1, 1, NULL, 1, NULL, 0),
@@ -1851,8 +1852,11 @@ INSERT INTO `ohrm_user_role_data_group` (`user_role_id`, `data_group_id`, `can_r
 
 (1, 46, 1, 1, 1, 1, 0),
 (2, 46, 0, 0, 0, 0, 0),
-(3, 46, 0, 0, 0, 0, 0);
+(3, 46, 0, 0, 0, 0, 0),
 
+(1, 47, 1, NULL, 1, NULL, 0),
+(2, 47, 0, 0, 0, 0, 0),
+(3, 47, 0, 0, 0, 0, 0);
 
 INSERT INTO `ohrm_job_category` (`name`) VALUES ('Officials and Managers'),
                                                 ('Professionals'),
