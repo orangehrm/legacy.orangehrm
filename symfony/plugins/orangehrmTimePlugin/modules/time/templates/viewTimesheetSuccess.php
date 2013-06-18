9d01fec7ccedf9f0d568c7fcf77a0d35d2f9cb4c
rtamarasinghe <rtamarasinghe@localhost>
2013-06-18 17:20:17 +0000

    Fixed php open tag

--- symfony/plugins/orangehrmTimePlugin/modules/time/templates/viewTimesheetSuccess.php
+++ symfony/plugins/orangehrmTimePlugin/modules/time/templates/viewTimesheetSuccess.php
@@ -31,7 +31,7 @@ $actionName = sfContext::getInstance()->getActionName();
         margin-bottom: 5px;
     }
 </style>
-<? if ($timesheetManagePermissions->canRead()) { ?>
+<?php if ($timesheetManagePermissions->canRead()) { ?>
     <?php if (isset($messageData[0])): ?>
         <div class="box timesheet">
             <div class="inner">
