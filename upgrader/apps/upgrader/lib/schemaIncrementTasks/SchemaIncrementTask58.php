d1fd64159eef54882542577493a95fad59d49d43
rtamarasinghe <rtamarasinghe@localhost>
2013-06-19 06:37:53 +0000

    Blank upgrader schema for 3.1.1

---
+++ upgrader/apps/upgrader/lib/schemaIncrementTasks/SchemaIncrementTask58.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+
+/**
+ * Changes from 3.1 to 3.1.1
+ * 
+ * 1) data group, screen changes
+ * 2) new table ohrm_data_group_screen
+ */
+class SchemaIncrementTask58 extends SchemaIncrementTask {
+    public $userInputs;
+
+    public function execute() {
+        $this->incrementNumber = 58;
+        parent::execute();
+
+        $result = array();
+
+        foreach ($this->sql as $sql) {
+            $result[] = $this->upgradeUtility->executeSql($sql);
+        }
+
+        $this->checkTransactionComplete($result);
+        $this->updateOhrmUpgradeInfo($this->transactionComplete, $this->incrementNumber);
+        $this->upgradeUtility->finalizeTransaction($this->transactionComplete);
+        $this->upgradeUtility->closeDbConnection();
+    }
+
+    public function getUserInputWidgets() {
+        
+    }
+
+    public function setUserInputs() {
+        
+    }
+    
+    public function loadSql() {
+
+        $sql = array();   
+                
+        $this->sql = $sql;
+    }
+    
+    public function getNotes() {        
+    }
+}
+
+
